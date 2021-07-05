ada8c25bbefe5f78febc8782557a2bb5840da066
ardha27 <ardhasemaranatha@gmail.com>
2021-07-06 00:41:36 +0800

    kripto komen

--- DECODER_VIGENERE.C
+++ DECODER_VIGENERE.C
@@ -7,26 +7,24 @@ int main(){
 	char cipher[MAX];
 	char key[MAX];
 	char k[MAX];
-//	CHAR k[MAX];
-	printf("Enter the string you want to decode: ");
-	gets(cipher);
+	printf("Enter the string you want to decode: ");		
+	gets(cipher);											//input string cipher
 	printf("\nEnter the key: ");
-	gets(key);
+	gets(key);												//input key
 	char get[MAX];
-	if(strlen(cipher)>strlen(key)){
-		while(cipher[i]!='\0'){
+	if(strlen(cipher)>strlen(key)){							//membandingkan cipher dan key 
+		while(cipher[i]!='\0'){								//yang panjang text melebihi key
 			get[t]=key[i%strlen(key)];
-//			printf("%c ",get[t]);
 			i++;
 			t++;
 		}
 	}
 	t=0;i=0;
-	if(strlen(cipher)<=strlen(key)){
-		get[t]=key[i];
+	if(strlen(cipher)<=strlen(key)){						//membandingkan cipher dan key
+		get[t]=key[i];										//panjang text kurang dari key
 	}
 	i=0;t=0;
-	while(key[t]!='\0'){
+	while(key[t]!='\0'){									//jika string bukan huruf/simbol ASCII 65-123
 		if((int)(key[t])<65 || (int)(key[t])>123){
 			printf("Invalid String");
 			return 1;
@@ -36,37 +34,35 @@ int main(){
 	i=0;t=0;
 	while(get[t]!='\0'){
 	
-		if((int)(get[t])>=65 && (int)(get[t])<=90){
+		if((int)(get[t])>=65 && (int)(get[t])<=90){			//Menghitung key untuk huruf Kapital
 			k[i]=get[t]-65;
 		}
-		else if((int)(get[t])>=97 && (int)(get[t])<=122){
+		else if((int)(get[t])>=97 && (int)(get[t])<=122){	//Menghitung key untuk huruf non kapital
 			k[i]=get[t]-97;
 		}
 		t++;
 		i++;
 	}
-//	}
 	t=0;i=0;
-//	rem=k%26;
 	int length=strlen(cipher);
 	while(cipher[i]!='\0'){
-		if((int)(cipher[i])<65 || (int)(cipher[i])>123){
+		if((int)(cipher[i])<65 || (int)(cipher[i])>123){	//Jika bukan alfabet maka akan di skip dekripsinya
 			plain[j]=(int)(cipher[i]);
-			--t;	//for rest chars escape
+			--t;
 		}
-		else if((int)(cipher[i])>=65 && (int)(cipher[i])<=90){
-			if((int)(cipher[i])-(int)(k[t])<=90){
+		else if((int)(cipher[i])>=65 && (int)(cipher[i])<=90){      //dekripsi huruf kapital
+			if((int)(cipher[i])-(int)(k[t])<=90){					//dekripsi huruf sampai Z
 				plain[j]=((int)(cipher[i])-((int)(k[t])));
 			}
-			if((int)(cipher[i])-(int)(k[t])<65){
+			if((int)(cipher[i])-(int)(k[t])<65){					//dekripsi huruf melebihi Z
 				plain[j]=((int)(cipher[i])-((int)(k[t])))+26;
 			}
 		}
-		else if((int)(cipher[i])>=97 && (int)(cipher[i])<=122){
-			if((int)(cipher[i])-(int)(k[t])<=122){
+		else if((int)(cipher[i])>=97 && (int)(cipher[i])<=122){		//dekripsi huruf non kapital
+			if((int)(cipher[i])-(int)(k[t])<=122){					//dekripsi huruf sampai z
 				plain[j]=((int)(cipher[i])-((int)(k[t])));
 			}
-			if((int)(cipher[i])-(int)(k[t])<97){
+			if((int)(cipher[i])-(int)(k[t])<97){					//dekripsi huruf melebihi Z
 				plain[j]=((int)(cipher[i])-((int)(k[t])))+26;
 			}
 		}
--- ENCODER_VIGENERE.c
+++ ENCODER_VIGENERE.c
@@ -8,26 +8,24 @@ int main(){
 	char cipher[MAX];
 	char key[MAX];
 	char k[MAX];
-//	CHAR k[MAX];
 	printf("Enter the string you want to encode: ");
-	gets(plain);
+	gets(plain);   												//mengambil input string
 	printf("\nEnter the key: ");
-	gets(key);
+	gets(key);     												//mengambil input key
 	char get[MAX];
-	if(strlen(plain)>strlen(key)){
-		while(plain[i]!='\0'){
+	if(strlen(plain)>strlen(key)){   							//membandingkan key dengan plaintext
+		while(plain[i]!='\0'){									//yang panjang text melebihi key
 			get[t]=key[i%strlen(key)];
-//			printf("%c ",get[t]);
 			i++;
 			t++;
 		}
 	}
 	t=0;i=0;
-	if(strlen(cipher)<=strlen(key)){
-		get[t]=key[i];
+	if(strlen(cipher)<=strlen(key)){							//membandingkan cipher dan key
+		get[t]=key[i];											//panjang text kurang dari key
 	}
 	i=0;t=0;
-	while(key[t]!='\0'){
+	while(key[t]!='\0'){										//jika string bukan huruf/simbol ASCII 65-123
 		if((int)(key[t])<65 || (int)(key[t])>123){
 			printf("Invalid String");
 			return 1;
@@ -37,37 +35,35 @@ int main(){
 	i=0;t=0;
 	while(get[t]!='\0'){
 	
-		if((int)(get[t])>=65 && (int)(get[t])<=90){
+		if((int)(get[t])>=65 && (int)(get[t])<=90){				//Menghitung key untuk huruf Kapital
 			k[i]=get[t]-65;
 		}
-		else if((int)(get[t])>=97 && (int)(get[t])<=122){
+		else if((int)(get[t])>=97 && (int)(get[t])<=122){		//Menghitung key untuk huruf non kapital
 			k[i]=get[t]-97;
 		}
 		t++;
 		i++;
 	}
-//	}
 	t=0;i=0;
-//	rem=k%26;
 	int length=strlen(plain);
-	while(plain[i]!='\0'){
+	while(plain[i]!='\0'){										//Jika bukan alfabet maka akan di skip enkripsinya
 		if((int)(plain[i])<65 || (int)(plain[i])>123){
 			cipher[j]=(int)(plain[i]);
 			--t;	//for rest chars escape
 		}
-		else if((int)(plain[i])>=65 && (int)(plain[i])<=90){
-			if((int)(plain[i])+(int)(k[t])<=90){
+		else if((int)(plain[i])>=65 && (int)(plain[i])<=90){	//enkripsi huruf kapital
+			if((int)(plain[i])+(int)(k[t])<=90){				//enkripsi huruf sampai Z
 				cipher[j]=((int)(plain[i])+((int)(k[t])));
 			}
-			if((int)(plain[i])+(int)(k[t])>90){
+			if((int)(plain[i])+(int)(k[t])>90){					//enkripsi huruf melebihi Z
 				cipher[j]=((int)(plain[i])+((int)(k[t])))-26;
 			}
 		}
-		else if((int)(plain[i])>=97 && (int)(plain[i])<=122){
-			if((int)(plain[i])+(int)(k[t])<=122){
+		else if((int)(plain[i])>=97 && (int)(plain[i])<=122){	//enkripsi huruf non kapital
+			if((int)(plain[i])+(int)(k[t])<=122){				//enkripsi huruf sampai Z
 				cipher[j]=((int)(plain[i])+((int)(k[t])));
 			}
-			if((int)(plain[i])+(int)(k[t])>122){
+			if((int)(plain[i])+(int)(k[t])>122){				//enkripsi huruf melebihi Z
 				cipher[j]=((int)(plain[i])+((int)(k[t])))-26;
 			}
 		}
@@ -77,7 +73,7 @@ int main(){
 		
 	}
 	cipher[j]='\0';
-	printf("%s\n",plain);
+	printf("%s\n",plain);	
 	printf("%s\n",cipher);
 	
 	return 0;
